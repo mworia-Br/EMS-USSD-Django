3569a36e9f5aa83dafa4a9b0e5e80a3c71513da8
Brian Nganga <briannganga70@gmail.com>
2022-10-26 20:08:02 +0300

    done

--- core/models.py
+++ core/models.py
@@ -12,20 +12,14 @@ class Service(models.Model):
         ("SUA", "SUA"),
     ]
     SERVICE_CATEGORY=models.CharField(max_length = 250, choices = SERVICECATEGORY_CHOICES, default = 'Awaiting confirmation')
-    service_name=models.CharField(
-        max_length=100
-    )
-    start=models.CharField(
-        max_length=250
-    )
-    finish=models.CharField(
-        max_length=250
-    )
+    service_name=models.CharField(max_length=100)
+    start=models.CharField(max_length=250)
+    finish=models.CharField(max_length=250)
     #price = models.FloatField()
     identifiers=models.IntegerField() #equivalent to seat number in a bus
-    is_available=models.BooleanField(
-        default=True
-    )
+    is_available=models.BooleanField(default=True)
+
+    
     def __str__(self) -> str:
         return self.service_name + " " + self.SERVICE_CATEGORY
 
@@ -42,11 +36,9 @@ class Reporting(models.Model):
     identifier=models.IntegerField(default=0)
     date = models.DateTimeField(auto_now_add=True)
     departure=models.DateTimeField(null=True, blank=True)
-    status = models.CharField(
-        max_length = 250,
-        choices = STATUS_CHOICES,
-        default = 'Awaiting confirmation'
-        )
+    status = models.CharField(max_length = 250, choices = STATUS_CHOICES, default = 'Awaiting confirmation')
+
+
     def __str__(self) -> str:
         return self.SERVICECATEGORY + " " + self.customer + " " + self.status
 
