feff9e65c4fad1e002626043176aac61a6aa864b
Brian Nganga <briannganga70@gmail.com>
2022-10-28 16:30:15 +0300

    static files

--- EMSUSSD/settings.py
+++ EMSUSSD/settings.py
@@ -121,7 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'EMSUSSD/static/')
+]
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
