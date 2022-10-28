365042dd81b70ab061375706c8fbbf9f93876c99
Brian Nganga <briannganga70@gmail.com>
2022-10-28 11:09:46 +0300

    secure

--- EMSUSSD/settings.py
+++ EMSUSSD/settings.py
@@ -9,18 +9,23 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# the load_dotenv function gets the environment variables defined in .env file
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h&@6&n)$0li4p@j#lsw2f^*sy$p@(i4hkty)ear3*s8k)7nc8v'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+#SECRET_KEY = 'django-insecure-h&@6&n)$0li4p@j#lsw2f^*sy$p@(i4hkty)ear3*s8k)7nc8v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
