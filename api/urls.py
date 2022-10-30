83d86d5184718956c10059e1668851d3f2b16b24
Brian Nganga <briannganga70@gmail.com>
2022-10-30 12:35:51 +0300

    working

--- api/urls.py
+++ api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
+    path('reporting-list/', views.reportingList, name="reporting-list"),
   ]
