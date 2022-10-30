88312a0dd7cd4482110e0b8dbbe0b4fd5335ab6b
Brian Nganga <briannganga70@gmail.com>
2022-10-30 11:39:19 +0300

    done

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ReportingSerializer
+from core.models import Reporting
+"""
+API Overview
+"""
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List' : '/reporting-list/',
+        'Detail View' : '/reporting-detail/<str:pk>/',
+        'Create' : '/reporting-create/',
+        'Update' : '/reporting-update/<str:pk>/',
+        #'Delete' : '/reporting-delete/<str:pk>/',
+    }
+    return Response(api_urls)
