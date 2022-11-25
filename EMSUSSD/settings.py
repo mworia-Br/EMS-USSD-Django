6cb27a1587cca07e009db8ac6f9cfc9275f0d917
Brian Nganga <briannganga70@gmail.com>
2022-11-25 13:28:17 +0300

    heroku settings

--- EMSUSSD/settings.py
+++ EMSUSSD/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'EMSUSSD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -93,7 +93,7 @@ DATABASES = {
 """
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
