9542a420ef80643bf749cc45c7ff04ba1f0f6a7e
Brian Nganga <briannganga70@gmail.com>
2023-02-25 20:34:52 +0300

    apisecured

--- EMSUSSD/settings.py
+++ EMSUSSD/settings.py
@@ -49,8 +49,15 @@ INSTALLED_APPS = [
     'core',
     'crimemap',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+} 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
